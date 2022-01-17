120cc7f149733d8cee6a32df65fadf9291ed3ac7
Mahmud <mahmud.salomov.salomov@gmail.com>
2022-01-17 11:54:54 +0500

    Initial commit

---
+++ src/main/java/uz/neft/liting/LitingApplication.java
@@ -0,0 +1,13 @@
+package uz.neft.liting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LitingApplication.class, args);
+    }
+
+}
