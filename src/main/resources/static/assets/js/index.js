cf322692a6225a3ea5eea2990db00792cd82f778
Nodirbotirov <nbotirov2000@gmail.comgit config --global user.email nbotirov2000@gmail.com>
2022-02-10 11:12:30 +0500

    Project qushildi barcha metodlari !!!

--- src/main/resources/static/assets/js/index.js
+++ src/main/resources/static/assets/js/index.js
@@ -76,7 +76,9 @@ function partnerComment() {
                     "                            </div>\n" +
                     "                        </div>";
             })
-            document.getElementById("swiper-root").innerHTML=out;
+            if (out !== ""){
+                document.getElementById("swiper-root").innerHTML=out;
+            }
         })
         .catch(function (error) {
 
@@ -87,40 +89,43 @@ function employee() {
         .then(function (response){
             let out="";
             let data = response.data.object;
-            console.log(data)
-            data.forEach(d=>{
-                out+=" <div class=\"col-lg-3 col-sm-6\">\n" +
-                "                <div class=\"team-block p-relative mb-md-40 wow fadeInUp\" data-wow-duration=\"1s\" data-wow-delay=\"0.3s\">\n" +
-                "                    <div class=\"inner-box bx-wrapper\">\n" +
-                "                        <div class=\"image animate-img\">\n" +
-                "                            <img src='/api/file/photo/"+d.photo.hashId+"' alt=\"img\" class=\"full-width\">\n" +
-                "                            <div class=\"overlay-box\">\n" +
-                "                                <div class=\"overlay-inner p-relative full-height\">\n" +
-                "                                    <ul class=\"team-social-box custom\">\n" +
-                "                                        <li class=\"youtube\"><a href=\"#\" class=\"fab fa-youtube fs-16\" tabindex=\"0\"></a><span class=\"social-name fs-12 text-custom-white\">youtube</span></li>\n" +
-                "                                        <li class=\"linkedin\"><a href=\"#\" class=\"fab fa-linkedin fs-16\" tabindex=\"0\"></a><span class=\"social-name fs-12 text-custom-white\">linkedin</span></li>\n" +
-                "                                        <li class=\"facebook\"><a href=\"#\" class=\"fab fa-facebook-f fs-16\" tabindex=\"0\"></a><span class=\"social-name fs-12 text-custom-white\">facebook</span></li>\n" +
-                "                                        <li class=\"twitter\"><a href=\"#\" class=\"fab fa-twitter fs-16\" tabindex=\"0\"></a><span class=\"social-name fs-12 text-custom-white\">twitter</span></li>\n" +
-                "                                    </ul>\n" +
-                "                                </div>\n" +
-                "                            </div>\n" +
-                "                        </div>\n" +
-                "                        <div class=\"lower-content p-relative text-center\">\n" +
-                "                            <div class=\"icon-box fs-18 text-custom-white\">\n" +
-                "                                <span class=\"fas fa-cogs\"></span>\n" +
-                "                            </div>\n" +
-                "                            <h4><a href=\"#\" class=\"fw-600 fs-20\" tabindex=\"0\">"+d.full_name+"</a></h4>\n" +
-                "                            <p class=\"designation text-custom-white mb-xl-20\">"+d.position+"</p>\n" +
-                "                        </div>\n" +
-                "                    </div>\n" +
-                "                </div>\n" +
-                "            </div>"
-            })
-            document.getElementById("row").innerHTML=out;
+                console.log(data)
+            data.forEach(d => {
+                    out += " <div class=\"col-lg-3 col-sm-6\">\n" +
+                        "                <div class=\"team-block p-relative mb-md-40 wow fadeInUp\" data-wow-duration=\"1s\" data-wow-delay=\"0.3s\">\n" +
+                        "                    <div class=\"inner-box bx-wrapper\">\n" +
+                        "                        <div class=\"image animate-img\">\n" +
+                        "                            <img src='/api/file/photo/" + d.photo.hashId + "' alt=\"img\" class=\"full-width\">\n" +
+                        "                            <div class=\"overlay-box\">\n" +
+                        "                                <div class=\"overlay-inner p-relative full-height\">\n" +
+                        "                                    <ul class=\"team-social-box custom\">\n" +
+                        "                                        <li class=\"youtube\"><a href=\"#\" class=\"fab fa-youtube fs-16\" tabindex=\"0\"></a><span class=\"social-name fs-12 text-custom-white\">youtube</span></li>\n" +
+                        "                                        <li class=\"linkedin\"><a href=\"#\" class=\"fab fa-linkedin fs-16\" tabindex=\"0\"></a><span class=\"social-name fs-12 text-custom-white\">linkedin</span></li>\n" +
+                        "                                        <li class=\"facebook\"><a href=\"#\" class=\"fab fa-facebook-f fs-16\" tabindex=\"0\"></a><span class=\"social-name fs-12 text-custom-white\">facebook</span></li>\n" +
+                        "                                        <li class=\"twitter\"><a href=\"#\" class=\"fab fa-twitter fs-16\" tabindex=\"0\"></a><span class=\"social-name fs-12 text-custom-white\">twitter</span></li>\n" +
+                        "                                    </ul>\n" +
+                        "                                </div>\n" +
+                        "                            </div>\n" +
+                        "                        </div>\n" +
+                        "                        <div class=\"lower-content p-relative text-center\">\n" +
+                        "                            <div class=\"icon-box fs-18 text-custom-white\">\n" +
+                        "                                <span class=\"fas fa-cogs\"></span>\n" +
+                        "                            </div>\n" +
+                        "                            <h4><a href=\"#\" class=\"fw-600 fs-20\" tabindex=\"0\">" + d.full_name + "</a></h4>\n" +
+                        "                            <p class=\"designation text-custom-white mb-xl-20\">" + d.position + "</p>\n" +
+                        "                        </div>\n" +
+                        "                    </div>\n" +
+                        "                </div>\n" +
+                        "            </div>"
+                })
+            if (out !== ""){
+                document.getElementById("row").innerHTML=out;
+            }
         })
         .catch(function (error){
 
         })
+
 }
 
 partnerComment()
